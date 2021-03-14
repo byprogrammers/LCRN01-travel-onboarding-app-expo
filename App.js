26a03be4e5c1fcea91e1058c9fb29f2ad02a34f6
Vincent <booncheng89@gmail.com>
2021-03-14 15:54:16 +0800

    Port over to Expo

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// screens
+import {
+    OnBoarding,
+} from "./app/screens/";
+
+import { useFonts } from 'expo-font';
+
+// screen for stack & tabs
+const Stack = createStackNavigator();
+const App = () => {
+    const [loaded] = useFonts({
+        "Roboto-Black" : require('./app/assets/fonts/Roboto-Black.ttf'),
+        "Roboto-Bold" : require('./app/assets/fonts/Roboto-Bold.ttf'),
+        "Roboto-Regular" : require('./app/assets/fonts/Roboto-Regular.ttf'),
+    })
+
+    if(!loaded){
+        return null;
+    }
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {/* Onboarding screen */}
+                <Stack.Screen name="OnBoarding" component={OnBoarding} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default () => {
+    return <App />;
+};
